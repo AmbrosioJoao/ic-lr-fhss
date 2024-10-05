d7bfbdda1f2765f00ea6277aabe0e7e912b15c83
AmbrosioJoao <joaoambrosio@alunos.utfpr.edu.br>
2024-10-05 13:38:49 -0300

    Start of the AoI graph attempt

---
+++ aoi.py
@@ -0,0 +1,17 @@
+"****************************"
+
+"Bibliotecas para os gráficos"
+from lrfhss.run import *
+import time
+from joblib import Parallel, delayed
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+import pickle
+import pandas as pd
+from matplotlib.ticker import EngFormatter
+import scienceplots
+import matplotlib.pyplot as plt 
+
+"***********AoI************"
+Aoi_medio = 0
